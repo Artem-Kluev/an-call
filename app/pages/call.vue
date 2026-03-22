7bbe53d08d84d0f1b6e970227b380470899363db
Kliuiev Artem <mr.kluev9999@gmail.com>
2026-03-22 14:19:29 +0200

    build

--- app/pages/call.vue
+++ app/pages/call.vue
@@ -24,8 +24,8 @@ onMounted(() => {
 });
 </script>
 
-<template>wwwwwwwwwwwwwwww
-  <UContainer class="bg-red-500 relative flex min-h-screen max-w-md flex-col gap-8 py-10">
+<template>
+  <UContainer class="bg-background relative flex min-h-screen max-w-md flex-col gap-8 py-10">
     <CallSearching v-if="state === 'searching'" @cancel="cancelSearch" />
     <CallActive v-else-if="state === 'active'" @end="endCall" />
     <CallDecision v-else-if="state === 'decision'" @choice="makeDecision" />
